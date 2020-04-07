1834499ba9ae75f2f16ce2e0d0bc62588549d459
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-07 14:49:26 -0600

    user sets

--- src/Components/Socket/Socket.js
+++ src/Components/Socket/Socket.js
@@ -8,15 +8,28 @@ let socket;
 
 function Socket (props) {
     const [rooms, setRooms] = useState([])
-    const [user, setUser] = useState([{name:''}])
+    const [user, setUser] = useState('test')
+    const [state, setState] = useState({
+        username: "",
+        message: "",
+        chat: [],
+        endpoint: "http://localhost:4420",
+      });
     
-    useEffect(() => {
-        socket = io('localhost:4420')
-        socket.emit('join',setUser({name:`${socket.id}`}))
-        
-        console.log(user)
-      }, ['localhost:4420'])
+      const [socket, setSocket] = useState(io(state.endpoint));
+    
+      useEffect(() => {
+        socket.on('connect', () => join());
 
+          socket.emit('join',)
+          
+          console.log(user)
+        }, [])
+        let join = () => {
+        console.log(socket)
+        setUser(socket.id)
+      }
+        console.log(user)
      
 
 
@@ -26,6 +39,7 @@ function Socket (props) {
         <>
         <div>Sockets</div>
         <input placeholder='Room Name'></input>
+        <button onClick={() => console.log(user)}></button>
         <button onClick={() => socket.emit('queue', console.log(socket.id))}>Join Queue</button>
         <button onClick={() => socket.emit('disconnect',console.log('hit disconnect')),() => {setRooms('')}}>Leave Room</button>
         </>
