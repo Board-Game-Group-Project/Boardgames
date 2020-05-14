5d0f4c6a684880667b66b55c187cbac168efadb9
DogSniffers <DogSniffers@users.noreply.github.com>
2020-05-14 01:19:01 -0600

    chess also added, css styling also bunk

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Landing from './Components/Landing/Landing'
 import Chess from './Components/Chess/components/game'
+import LocalChess from './Components/LocalChess/components/game'
 import Scoreboard from './Components/Scoreboard/Scoreboard'
 import Profile from './Components/Profile/Profile'
 import TicTacToe from './Components/TicTacToe/TicTacToe'
@@ -12,6 +13,7 @@ export default (
     <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/chess" component={Chess} />
+        <Route path="/chesslocal" component={LocalChess}/>
         <Route path="/tictactoe" component={TicTacToe}/>
         <Route path="/tictactoelocal" component={LocalTicTacToe}/>
         <Route path="/scoreboard" component={Scoreboard} />
